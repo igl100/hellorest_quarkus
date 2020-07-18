9fa53940a02ea0429c66a017d33720dbbc42c757
Isaac Galvan <igl100@redhat.com>
2020-07-18 18:40:15 -0500

    Initial commit

---
+++ src/test/java/com/igalvan/services/rest/resttest/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.igalvan.services.rest.resttest;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
